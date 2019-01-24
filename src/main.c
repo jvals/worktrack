b52e5cfc05539809def336698555371a7ff9c343
jvals <jvals@users.noreply.github.com>
2019-01-24 09:44:45 +0100

    Add comments in main

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@ int main() {
   int err = 0;
   server_t server = {0};
 
-  err = server_listen(&server);
+  err = server_listen(&server);  // Open TCP Socket
   if (err) {
     LOGGER(FATAL, "Failed to listen on address 0.0.0.0:%d\n", PORT);
     return err;
   }
 
   while (1) {
-    err = server_accept(&server);
+    err = server_accept(&server); // Check TCP Socket for connections
     if (err) {
       LOGGER(FATAL, "Failed accepting connection\n", "");
       return err;
