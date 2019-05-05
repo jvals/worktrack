ef15191a8b8b4c56fd6288b2f029bc0f6fb98297
jvals <jvals@users.noreply.github.com>
2019-05-05 15:21:02 +0200

    Mock time()

--- test/test_time_service.c
+++ test/test_time_service.c
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #include "../src/time_entry_t.h"
 
 void test_time_service_create_entry() {
@@ -14,6 +16,23 @@ void test_time_service_get_total_of_diffs() {
 
 void __wrap_safe_new_entry(time_entry_t time_entry) {
   return;
+}
+
+time_t time(time_t *tloc) {
+  struct tm may1st;
+  may1st.tm_sec = 0;
+  may1st.tm_min = 0;
+  may1st.tm_hour = 0;
+  may1st.tm_mday = 1;
+  may1st.tm_mon = 4;
+  may1st.tm_year = 2019 - 1900;
+  may1st.tm_isdst = 1;
+
+  if (tloc != NULL) {
+    *tloc = mktime(&may1st);
+  }
+
+  return mktime(&may1st);
 }
 
 int  main() {
