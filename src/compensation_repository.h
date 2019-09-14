581c5fa8ef41ddb30a89a39b69ddc7097bce6f38
jvals <jvals@users.noreply.github.com>
2019-09-14 12:17:22 +0200

    Add compensation sum function to repo headers

--- src/compensation_repository.h
+++ src/compensation_repository.h
@@ -1,4 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
 #include "compensation_entry_t.h"
 
 void create_compensation_table(void);
 void compensation_create_entry(compensation_entry_t);
+void compensation_get_sum(uint64_t*);
