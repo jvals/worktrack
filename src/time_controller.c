045f8f05aefcf6d8baea9ccbdd70baf8760de01d
jvals <jvals@users.noreply.github.com>
2019-09-15 14:36:31 +0200

    Initialise response_t struct to 0

--- src/time_controller.c
+++ src/time_controller.c
@@ -10,7 +10,7 @@
 
 response_t get_total_time(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   uint64_t total_diff = 0;
   total_diff = time_service_get_total_of_diffs();
@@ -28,7 +28,7 @@ response_t get_total_time(request_t req) {
 
 response_t start_time(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   time_service_create_entry();
 
@@ -43,7 +43,7 @@ response_t start_time(request_t req) {
 
 response_t stop_time(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   time_service_stop_latest_entry();
 
@@ -58,7 +58,7 @@ response_t stop_time(request_t req) {
 
 response_t get_todays_time(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   uint64_t todays_diff = 0;
   todays_diff = time_service_get_todays_diff();
@@ -76,7 +76,7 @@ response_t get_todays_time(request_t req) {
 
 response_t get_overtime(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   uint64_t overtime = 0;
   overtime = time_service_get_overtime_with_compensation();
@@ -95,7 +95,7 @@ response_t get_overtime(request_t req) {
 
 response_t get_work_in_progress(request_t req) {
   UNUSED(req);
-  response_t response;
+  response_t response = {0};
 
   bool work_in_progress = false;
   work_in_progress = time_service_unfinished_work();
