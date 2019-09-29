ff9e1882b8b620c51e77e931ec9f718da9db0615
jvals <jvals@users.noreply.github.com>
2019-09-29 19:20:25 +0200

    Add script for cross compiling for arm on Ubuntu

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+sudo apt update
+
+# Install the compiler used for cross compiling
+sudo apt install gcc-arm-linux-gnueabihf -y
+
+# Update sources with armhf ports
+cat << EOF | sudo tee /etc/apt/sources.list
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ xenial main multiverse restricted universe
+deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports/ xenial main multiverse restricted universe
+deb [arch=armhf] http://ports.ubuntu.com/ubuntu-ports/ xenial-updates main multiverse restricted universe
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ xenial-updates main multiverse restricted universe
+deb [arch=amd64] http://security.ubuntu.com/ubuntu/ xenial-security main multiverse restricted universe
+EOF
+sudo dpkg --add-architecture armhf
+sudo apt update
+
+# We need the arm version of libsqlite3-dev
+sudo apt install libsqlite3-dev:armhf -y
+
+# Make clogger
+cd clogger
+CC=arm-linux-gnueabihf-gcc CFLAGS='-O3 -Wall -Wextra -Wpedantic -march=armv7 -D LOG_ALL' make 
+cd -
+
+# Make project
+CC=arm-linux-gnueabihf-gcc \
+  LD=arm-linux-gnueabihf-gcc\
+  LDFLAGS='-Lclogger -static -llogger -lsqlite3 -ldl -lpthread'\
+  CFLAGS='-O3 -Wall -Wextra -Wpedantic -march=armv7 -std=gnu11 -D LOG_INFO -Iclogger'\
+  make
+
+# Test the arm binary
+sudo apt install qemu-user -y
+sudo apt install sqlite3 -y
+./run_e2e_tests.sh
+
